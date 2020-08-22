4996f3a90073b548702e238d5eef42223bb33a05
DanielGuarchaj <gtdanniel@gmail.com>
2020-08-22 16:29:43 -0600

    change name

---
+++ connection_db/connection.php
@@ -0,0 +1,17 @@
+<?php
+    $host = "localhost";
+    $database = "ejercicio_dw";
+    $username = "root";
+    $password = "";
+
+    $conn = mysqli_connect($host,$username,$password,$database);
+
+    if(!$conn){
+        die("Connection falided: " . mysqli_connect_error());
+    }
+
+    // echo "Connected successfully";
+
+    mysqli_close($conn);
+
+?>
