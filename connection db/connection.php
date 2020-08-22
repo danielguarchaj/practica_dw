854d14849c887181862848d51979c3db1ff93f8b
bgomez <brandon@weclickdigital.com>
2020-08-22 15:09:42 -0600

    connection database

---
+++ connection db/connection.php
@@ -0,0 +1,17 @@
+<?php
+    $host = "localhost";
+    $database = "tarea_desarrollo";
+    $username = "root";
+    $password = "";
+
+    $conn = mysqli_connect($host,$username,$password,$database);
+
+    if(!$conn){
+        die("Connection falided: " . mysqli_connect_error());
+    }
+
+    echo "Connected successfully";
+
+    mysqli_close($conn);
+
+?>
